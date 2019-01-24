0b951aef9ab28b062e5e0a8c5f3bfcf3548d5acd
Kyle Hickinson <git@kylehickinson.com>
2019-01-24 14:55:04 -0500

    Fix building on iOS by setting a higher versioned minimum iOS version

--- build.rs
+++ build.rs
@@ -17,6 +17,16 @@ fn main() {
         .cpp(true) // Switch to C++ library compilation.
         .flag("-std=c++11");
 
+    // For iOS targets we actually need to override the default minimum ios version thats
+    // set in cc::Build due to needing support for `thread_local`
+    if let Ok(target) = env::var("TARGET") {
+        if target == "aarch64-apple-ios" {
+            cc_build.flag("-miphoneos-version-min=12.0");
+        } else if target == "x86_64-apple-ios" {
+            cc_build.flag("-miphonesimulator-version-min=12.0");
+        }
+    }
+
     if let Ok(ndebug) = ndebug {
         cc_build.flag(&("-DNDEBUG=".to_owned() + &ndebug));
     }
